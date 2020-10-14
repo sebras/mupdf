527e3cfe8f3e5328f7ea54fc30e32dbd542977bd
Sebastian Rasmussen <sebras@gmail.com>
2020-10-14 18:04:02 +0800

    x

--- source/pdf/pdf-write.c
+++ source/pdf/pdf-write.c
@@ -2428,7 +2428,6 @@ dowriteobject(fz_context *ctx, pdf_document *doc, pdf_write_state *opts, int num
 			opts->ofs_list[num] = fz_tell_output(ctx, opts->out);
 			writeobject(ctx, doc, opts, num, opts->gen_list[num], 1, num == opts->crypt_object_number);
 		}
-		opts->use_list[num] = 1;
 	}
 	else
 		opts->use_list[num] = 0;
